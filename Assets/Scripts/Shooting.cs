c80b6e026397685429e0c3e4b2e482b7a888a140
Sajgon <mateusz.arkadiusz.wosik@gmail.com>
2023-03-17 20:07:59 +0100

    - added reload and ammo capacity for weapon - added upgrades to reduce reload time, increase fire rate and increase ammo capacity - no UI for ammo yet

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -8,7 +8,11 @@ public class Shooting : MonoBehaviour
     [SerializeField] private GameObject _muzzleFlash;
     [SerializeField] private Transform _muzzleFlashPos;
     [SerializeField] private float _fireRate;
+    [SerializeField] private float _reloadTime;
     [SerializeField] private float _timer;
+    [SerializeField] private int _maxAmmoCapacity;
+    [SerializeField] private int _ammoCapacity;
+    [SerializeField] private bool _isReloading;
 
     private void Start()
     {
@@ -18,20 +22,34 @@ public class Shooting : MonoBehaviour
     private void Update()
     {
         _timer += Time.deltaTime;
+        CheckAmmoCapacity();
 
         Shot();
     }
 
     void Shot()
     {
-        if (Input.GetMouseButton(0) && _timer > _fireRate && Time.timeScale > 0)
+        if (Input.GetMouseButton(0) && _timer > _fireRate && Time.timeScale > 0 && _ammoCapacity > 0)
         {
             _timer = 0;
+            _ammoCapacity--;
 
             Instantiate(_explosion, CalculateMouseToWorldPosition(), Quaternion.identity);
             Instantiate(_muzzleFlash, _muzzleFlashPos.position, _muzzleFlashPos.rotation);
         }
         
+    }
+
+    void CheckAmmoCapacity()
+    {
+        if (_ammoCapacity <= 0) _isReloading = true;
+        else _isReloading = false;
+        if (_isReloading) ReloadWeapon();
+    }
+
+    void ReloadWeapon()
+    {
+        if (_timer >= _reloadTime) _ammoCapacity = _maxAmmoCapacity;
     }
 
     Vector3 CalculateMouseToWorldPosition()
@@ -40,5 +58,21 @@ public class Shooting : MonoBehaviour
         screenToWorld.z = 0;
 
         return screenToWorld;
+    }
+
+    public void UpgradeFireRate()
+    {
+        _fireRate *= .9f;
+    }
+
+    public void UpgradeReloadTime()
+    {
+        _reloadTime *= .9f;
+    }
+
+    public void UpgradeAmmoCapacity()
+    {
+        _maxAmmoCapacity++;
+        _ammoCapacity++;
     }
 }
--- Assets/Scripts/Upgrades.cs
+++ Assets/Scripts/Upgrades.cs
@@ -1,17 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Upgrades : MonoBehaviour
 {
     [SerializeField] GameObject[] _towerObjects;
+    [SerializeField] Shooting _shootingScript;
+
+    [Header("Firerate Upgrade")]
+    [SerializeField] Button _fireRateButton;
+    [SerializeField] TextMeshProUGUI _fireRateButtonText;
+    [SerializeField] TextMeshProUGUI _fireRateCostText;
+    [SerializeField] int _fireRateLevel = 0;
+    [SerializeField] float _fireRateCost;
+
+    [Header("Reload Speed Upgrade")]
+    [SerializeField] Button _reloadTimeButton;
+    [SerializeField] TextMeshProUGUI _reloadTimeButtonText;
+    [SerializeField] TextMeshProUGUI _reloadTimeCostText;
+    [SerializeField] int _reloadTimeLevel = 0;
+    [SerializeField] float _reloadTimeCost;
+
+    [Header("Ammo Capacity Upgrade")]
+    [SerializeField] Button _ammoCapacityButton;
+    [SerializeField] TextMeshProUGUI _ammoCapacityButtonText;
+    [SerializeField] TextMeshProUGUI _ammoCapacityCostText;
+    [SerializeField] int _ammoCapacityLevel = 0;
+    [SerializeField] float _ammoCapacityCost;
+
+    private void Start()
+    {
+        _shootingScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Shooting>();
+    }
 
     public void UpgradeTower()
     {
-        if(Points.Instance.points >= 100)
+        if(Points.Instance.points >= 500)
         {
-            Points.Instance.points -= 100;
+            Points.Instance.points -= 500;
             int button = int.Parse(EventSystem.current.currentSelectedGameObject.name);
             _towerObjects[button].SetActive(true);
             EventSystem.current.currentSelectedGameObject.SetActive(false);
@@ -20,5 +49,59 @@ public class Upgrades : MonoBehaviour
             GameManager.Instance.towerHealth += 100f;
             UIManager.Instance.UpdateTowerHealthSlider();
         }
+    }
+
+    public void UpgradeFireRate()
+    {
+        if (_fireRateLevel < 20 && Points.Instance.points > _fireRateCost)
+        {
+            _shootingScript.UpgradeFireRate();
+            Points.Instance.points -= (int)_fireRateCost;
+            _fireRateLevel++;
+            _fireRateCost *= 1.4f;
+            _fireRateCostText.text = ((int)_fireRateCost).ToString();      
+            UIManager.Instance.UpdateGoldValue();
+            _fireRateButtonText.text = _fireRateLevel.ToString();
+            if( _fireRateLevel >= 20)
+            {
+                _fireRateButton.interactable = false;
+                _fireRateButtonText.text = "MAX";
+                _fireRateCostText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void UpgradeReloadTime()
+    {
+        if (_reloadTimeLevel < 20 && Points.Instance.points > _reloadTimeCost)
+        {
+            _shootingScript.UpgradeReloadTime();
+            Points.Instance.points -= (int)_reloadTimeCost;
+            _reloadTimeLevel++;
+            _reloadTimeCost *= 1.4f;
+            _reloadTimeCostText.text = ((int)_reloadTimeCost).ToString();     
+            UIManager.Instance.UpdateGoldValue();
+            _reloadTimeButtonText.text = _reloadTimeLevel.ToString();
+            if(_reloadTimeLevel >= 20)
+            {
+                _reloadTimeButton.interactable = false;
+                _reloadTimeButtonText.text = "MAX";
+                _reloadTimeCostText.gameObject.SetActive(false);
+            }
+        }      
+    }
+
+    public void UpgradeAmmoCapacity()
+    {
+        if(Points.Instance.points > _ammoCapacityCost)
+        {
+            _shootingScript.UpgradeAmmoCapacity();
+            Points.Instance.points -= (int)_ammoCapacityCost;
+            _ammoCapacityLevel++;
+            _ammoCapacityCost += 100;
+            _ammoCapacityCostText.text = ((int)_ammoCapacityCost).ToString();     
+            UIManager.Instance.UpdateGoldValue();
+            _ammoCapacityButtonText.text = _ammoCapacityLevel.ToString();
+        }  
     }
 }
